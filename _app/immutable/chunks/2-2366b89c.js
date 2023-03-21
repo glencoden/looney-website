aaeb1a3d45cdb18d57e54e684189711f9a6f8b58
glencoden <80849247+glencoden@users.noreply.github.com>
2023-03-21 14:54:51 +0000

    Build: (a5c950e) add playsinline video attribute

--- _app/immutable/chunks/2-2366b89c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-df298f6c.js";export{t as component};
---
+++ _app/immutable/chunks/2-951ce9b9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-50ae56ff.js";export{t as component};
