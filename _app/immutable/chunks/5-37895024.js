88c7b950c565ab5ab84a2de7577278777d677141
glencoden <80849247+glencoden@users.noreply.github.com>
2023-04-25 10:31:05 +0000

    Build: (3bb59d9) rename static references directory

--- _app/immutable/chunks/5-37895024.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/references/_page.svelte-fb92d171.js";export{t as component};
---
+++ _app/immutable/chunks/5-813d41da.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/references/_page.svelte-ca7f95e5.js";export{t as component};
