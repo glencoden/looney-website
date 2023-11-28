22f3cf51951e5ce48125bd62c67758835af04b98
glencoden <80849247+glencoden@users.noreply.github.com>
2023-11-28 14:56:52 +0000

    Build: (dfabf39) extend max content width, update event list styles

--- _app/immutable/chunks/4-605a1eaa.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/events/_page.svelte-04f851a8.js";export{t as component};
---
+++ _app/immutable/chunks/4-66f39297.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/events/_page.svelte-5ecb1e3c.js";export{t as component};
