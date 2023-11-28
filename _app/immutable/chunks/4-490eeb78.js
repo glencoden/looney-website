2fb25c32e1858f5bf1a25089220d28992db5db27
glencoden <80849247+glencoden@users.noreply.github.com>
2023-11-28 14:59:03 +0000

    Build: (a0d57ec) Merge pull request #7 from glencoden/develop
    extend max content width, update event list styles

--- _app/immutable/chunks/4-490eeb78.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/events/_page.svelte-5678b13d.js";export{t as component};
---
+++ _app/immutable/chunks/4-bc3e8757.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/events/_page.svelte-e2e5be59.js";export{t as component};
