60b8262ba15701d0c6f45dfa29e92f8a433d6918
glen coden <info@simonmeyer.de>
2023-11-30 10:33:51 +0100

    explicit fetch post headers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
     onMount(() => {
         console.log(
-            '%cglencoden ❤️ version 1.2.1',
+            '%cglencoden ❤️ version 1.2.2',
             `font-size: 1rem;
             padding: 1rem;
             margin: 1rem 0;
