4d64a276be56d4f53742bff9dc2916f8521c5293
glencoden <80849247+glencoden@users.noreply.github.com>
2025-05-03 12:33:40 +0000

    Build: (79b21a0) feat(contact): smaller font for email address

--- _app/immutable/chunks/3-081d7fcb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/contact/_page.svelte-4a9594c0.js";export{t as component};
---
+++ _app/immutable/chunks/3-54156242.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/contact/_page.svelte-0f983843.js";export{t as component};
